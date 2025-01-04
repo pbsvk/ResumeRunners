68141a0d0fd026611fe25a7c6ce449eac4820c17
Bhaskara Padala <vamsikrishna2002@icloud.com>
2025-01-04 17:10:58 -0500

    Connected detailsfill with signin

---
+++ JS/detailsfill.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Open Sign In Page
+document.getElementById("signinw").addEventListener("click", function(){
+    window.location.href = "../HTML/signin.html";
+  });
