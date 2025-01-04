0e5451f76bd079030702163d1b1abd7814015d6f
Bhaskara Padala <vamsikrishna2002@icloud.com>
2025-01-04 17:04:28 -0500

    SignIn&SignUp-connected

---
+++ JS/signin.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Open Sign In Page
+document.getElementById("signupw").addEventListener("click", function(){
+    window.location.href = "../HTML/signup.html";
+  });
